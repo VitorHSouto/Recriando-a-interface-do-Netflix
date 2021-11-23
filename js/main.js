dc096cd2b79b4ca09afb05e2d26d5975a886c62d
VitorHSouto <vitor.souto@ufu.br>
2021-11-23 15:40:59 -0300

    Atts - Trailer principal inserido

--- js/main.js
+++ js/main.js
@@ -1,15 +1,37 @@
-// what should we do when scrolling occurs
-function runOnScroll(element) {
-    // not the most exciting thing, but a thing nonetheless
-    console.log(element);
-  };
-  
-  // grab elements as array, rather than as NodeList
-  const elements = document.getElementById('nav');
-  
-  // and then make each element do something on scroll
-    window.addEventListener(
-        "scroll",
-        () => runOnScroll(elements),
-        { passive: true }
-    );
+
+const nav = document.getElementById('nav');
+
+window.addEventListener('scroll', () => {
+if(window.scrollY >= 130){
+    nav.classList.add('nav__black');
+    nav.classList.remove('nav__transparent');
+}
+else{
+    nav.classList.add('nav__transparent');
+    nav.classList.remove('nav__black');
+}
+})
+
+function whenLoad(){
+    const nav = document.getElementById('nav');
+    nav.classList.add('nav__transparent');
+    console.log("Carregou!");
+}
+
+function trailer_start()
+{
+    var display = document.getElementById('bg_video');
+
+    var video = document.getElementById('bg_video');
+    // or video = $('.video-selector')[0];
+    video.pause();
+    video.currentTime = 0;
+    video.load();
+    display.style.display = "block";
+}
+
+function trailer_stop()
+{
+    var display = document.getElementById('bg_video');
+    display.style.display = "none";
+}
